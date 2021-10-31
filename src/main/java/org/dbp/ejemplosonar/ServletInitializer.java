683733a72c9977e4c0330edf9f815f07467a6431
blancoparis-tfc <blancoparis.tfc@gmail.com>
2021-10-31 21:05:42 +0100

    Excluir ficheros de la covertura

--- src/main/java/org/dbp/ejemplosonar/ServletInitializer.java
+++ src/main/java/org/dbp/ejemplosonar/ServletInitializer.java
@@ -3,6 +3,7 @@ package org.dbp.ejemplosonar;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@Generated
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
