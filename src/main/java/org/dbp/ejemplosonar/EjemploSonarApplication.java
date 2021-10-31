c2a117549bdaf5545d934f2e772e5c62f9424c27
blancoparis-tfc <blancoparis.tfc@gmail.com>
2021-10-31 15:08:48 +0100

    Guardar los datos

---
+++ src/main/java/org/dbp/ejemplosonar/EjemploSonarApplication.java
@@ -0,0 +1,13 @@
+package org.dbp.ejemplosonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploSonarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploSonarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dbp/ejemplosonar/EjemploSonarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dbp.ejemplosonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploSonarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
